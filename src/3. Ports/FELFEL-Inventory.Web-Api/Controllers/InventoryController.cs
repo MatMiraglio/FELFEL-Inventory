39b38f1d65f15a11f3582e3f0faf4651c0cb5ef7
Matias <miragliomatias@gmail.com>
2019-06-21 17:13:49 +0200

    defin new endpoints for inventory

---
+++ src/3. Ports/FELFEL-Inventory.Web-Api/Controllers/InventoryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FELFEL.UseCases.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FELFEL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IBatchRepository batchRepository;
+
+        public InventoryController(
+            IBatchRepository batchRepository
+            )
+        {
+            this.batchRepository = batchRepository;
+        }
+
+        // GET api/inventory/5
+        [HttpGet("{productId}")]
+        public IActionResult GetInventoryPerProduct(int productId)
+        {
+            var batches = batchRepository.GetInventoryPerProduct(productId);
+
+            return Ok(batches);
+        }
+
+        // GET api/inventory/freshness
+        [HttpGet("/freshness")]
+        public IActionResult GetInventoryFreshnessOverview()
+        {
+            var batches = batchRepository.GetAll();
+
+            return Ok(batches);
+        }
+    }
+}
